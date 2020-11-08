af72233b53ebec60518b5fd5c777a9bf3ee2a726
DieterClaessens67 <dieterclaessens3@hotmail.com>
2020-11-08 11:20:12 +0100

    fixed settings auth

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework.authtoken',
     'rest_framework',
     'myapi.apps.MyapiConfig',
     'django.contrib.admin',
@@ -40,6 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
